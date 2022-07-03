e70e5f3f57f75edca5d8d40d812396884799dbd5
TCOALTUNORGU <ogulcan.altunorgu@kktcell.com>
2022-07-03 23:31:18 +0300

    angular-letter-app added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'letter-app';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  getInputValue(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
